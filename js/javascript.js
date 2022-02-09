f3d28fc246a759bc6cef1ad61aaf11445fea16cc
Yauheni Kaspiarovich <kaspiarovichy@gmail.com>
2022-02-09 18:22:20 +0100

    work with mentor

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+// console.log("Привет")
+// // let a = 5;
+// // let a = 10;
+// // var a = 5;
+// // var a = 10;
+// // console.log(a)
+// if (true) {
+//     let a = 10 
+// }
+// console.log(a)
+
+let str = "abcde";
+alert(str[0]);
+alert(str[1]);
+alert(str[4]);
+
